36db8b8c859abe35fd5e20afe935d0ab017b6675
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2023-11-07 11:10:48 -0300

    refatoracao: classe EscolhaDoMenu removida

--- src/main/java/com/eyesonserver/terminal/Terminal.java
+++ src/main/java/com/eyesonserver/terminal/Terminal.java
@@ -1,31 +1,24 @@
 package com.eyesonserver.terminal;
 
 import com.eyesonserver.dao.maquina.ProcessoDAO;
-import com.eyesonserver.dao.maquina.ServidorDAO;
 import com.eyesonserver.dao.metrica.RegistroDAO;
 import com.eyesonserver.dao.negocio.UsuarioDAO;
-import com.eyesonserver.database.Conexao;
 import com.eyesonserver.model.maquina.Processo;
-import com.eyesonserver.model.maquina.Servidor;
 import com.eyesonserver.model.metrica.Registro;
 import com.eyesonserver.model.negocio.Usuario;
-import com.eyesonserver.terminal.EscolhadoDoMenu;
 import com.github.britooo.looca.api.core.Looca;
-import java.time.LocalDate;
+import com.github.britooo.looca.api.group.processos.ProcessoGrupo;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
-
-import com.github.britooo.looca.api.group.processos.ProcessoGrupo;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.sql.SQLOutput;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Terminal {
     //Aqui sera desenvolvida a interface de terminal da aplicacao
-    static EscolhadoDoMenu escolhadoDoMenu = new EscolhadoDoMenu();
+    static Integer escolhadoDoMenu = 0;
 
     public static void main(String[] args) {
 
@@ -45,7 +38,7 @@ public class Terminal {
                 """);
 
 
-        while (!escolhadoDoMenu.getEscolhaDoMenu().equals(6)){
+        while (!escolhadoDoMenu.equals(7)){
 
         System.out.print("Login: ");
         email = leitor.nextLine();
@@ -78,100 +71,83 @@ public class Terminal {
                         7. Sair
                         """);
                 System.out.print("Escolha: ");
-                escolhadoDoMenu.setEscolhaDoMenu(leitor.nextInt());
+                escolhadoDoMenu = leitor.nextInt();
 
-                if (escolhadoDoMenu.getEscolhaDoMenu().equals(1)){
-                    System.out.println("Opção (Disco) escolhida!");
-
-                    long discoTotal = looca.getGrupoDeDiscos().getVolumes().get(0).getTotal() / 1000000000;
-                    long discoDisponivel = looca.getGrupoDeDiscos().getVolumes().get(0).getDisponivel() / 1000000000;
-
-                    System.out.print("Porcentagem de uso: ");
-                    System.out.print((discoTotal - discoDisponivel) * 100 / discoTotal);
-                    System.out.println(" %");
-
-                } else if (escolhadoDoMenu.getEscolhaDoMenu().equals(2)) {
-                    System.out.println("Opção (Memória) escolhida! ");
-
-                    long memoriaTotal = looca.getMemoria().getTotal() / 100000000;
-                    long memoriaEmUso = looca.getMemoria().getEmUso() / 100000000;
-
-                    System.out.print("Porcentagem de uso: ");
-                    System.out.print(memoriaEmUso * 100 / memoriaTotal);
-                    System.out.println(" %");
-
-
-                } else if (escolhadoDoMenu.getEscolhaDoMenu().equals(3)) {
-                    System.out.println("Opção (Rede) escolhida! ");
-
-                    System.out.print("Bytes enviados: ");
-                    System.out.print(looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getBytesEnviados());
-                    System.out.println(" bytes");
-
-                    System.out.print("Bytes recebidos: ");
-                    System.out.print(looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getBytesRecebidos());
-                    System.out.println(" bytes");
-
-
-                } else if (escolhadoDoMenu.getEscolhaDoMenu().equals(4)) {
-                    System.out.println("Opção (CPU) escolhida!");
-
-                    System.out.print("Frequência de CPU: ");
-                    System.out.print(looca.getProcessador().getFrequencia() / 1000000000);
-                    System.out.println(" Hz");
-
-                } else if (escolhadoDoMenu.getEscolhaDoMenu().equals(5)) {
-                    System.out.println("Opção (Todos) escolhida! ");
-
-                    long discoTotal = looca.getGrupoDeDiscos().getVolumes().get(0).getTotal() / 1000000000;
-                    long discoDisponivel = looca.getGrupoDeDiscos().getVolumes().get(0).getDisponivel() / 1000000000;
-
-                    System.out.print("Porcentagem de uso do disco: ");
-                    System.out.print((discoTotal - discoDisponivel) * 100 / discoTotal);
-                    System.out.println(" %");
-
-                    long memoriaTotal = looca.getMemoria().getTotal() / 100000000;
-                    long memoriaEmUso = looca.getMemoria().getEmUso() / 100000000;
-
-                    System.out.print("Porcentagem de uso da memória: ");
-                    System.out.print(memoriaEmUso * 100 / memoriaTotal);
-                    System.out.println(" %");
-
-                    System.out.print("Bytes enviados: ");
-                    System.out.print(looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getBytesEnviados());
-                    System.out.println(" bytes");
-
-                    System.out.print("Bytes recebidos: ");
-                    System.out.print(looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getBytesRecebidos());
-                    System.out.println(" bytes");
-
-                    System.out.print("Frequência de CPU: ");
-                    System.out.print(looca.getProcessador().getFrequencia() / 1000000000);
-                    System.out.println(" Hz");
-
-
-
-
-                }
-                else if (escolhadoDoMenu.getEscolhaDoMenu().equals(6)) {
-                    System.out.println("Nome Processo; PID; % Uso CPU; % Uso RAM; MB utilizados; VRAM Utilizada");
-                    ProcessoGrupo prcs = looca.getGrupoDeProcessos();
-                    for (com.github.britooo.looca.api.group.processos.Processo processo : prcs.getProcessos()) {
-                        System.out.printf("%s; %d; %.1f; %.1f; %.1f\n",
-                                processo.getNome(),
-                                processo.getPid(),
-                                processo.getUsoCpu(),
-                                processo.getUsoMemoria(),
-                                processo.getBytesUtilizados() / Math.pow(10, 6),
-                                processo.getMemoriaVirtualUtilizada() / Math.pow(10, 6)
-                        );
+                switch (escolhadoDoMenu) {
+                    case 1 -> {
+                        System.out.println("Opção (Disco) escolhida!");
+                        long discoTotal = looca.getGrupoDeDiscos().getVolumes().get(0).getTotal() / 1000000000;
+                        long discoDisponivel = looca.getGrupoDeDiscos().getVolumes().get(0).getDisponivel() / 1000000000;
+                        System.out.print("Porcentagem de uso: ");
+                        System.out.print((discoTotal - discoDisponivel) * 100 / discoTotal);
+                        System.out.println(" %");
                     }
-                }
-                else if (escolhadoDoMenu.getEscolhaDoMenu().equals(7)) {
-                    System.out.println("Até mais!");
-                    Thread.currentThread().stop();
-                }else {
-                    System.out.println("Escolha inválida!");
+                    case 2 -> {
+                        System.out.println("Opção (Memória) escolhida! ");
+                        long memoriaTotal = looca.getMemoria().getTotal() / 100000000;
+                        long memoriaEmUso = looca.getMemoria().getEmUso() / 100000000;
+                        System.out.print("Porcentagem de uso: ");
+                        System.out.print(memoriaEmUso * 100 / memoriaTotal);
+                        System.out.println(" %");
+                    }
+                    case 3 -> {
+                        System.out.println("Opção (Rede) escolhida! ");
+                        System.out.print("Bytes enviados: ");
+                        System.out.print(looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getBytesEnviados());
+                        System.out.println(" bytes");
+                        System.out.print("Bytes recebidos: ");
+                        System.out.print(looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getBytesRecebidos());
+                        System.out.println(" bytes");
+                    }
+                    case 4 -> {
+                        System.out.println("Opção (CPU) escolhida!");
+                        System.out.print("Frequência de CPU: ");
+                        System.out.print(looca.getProcessador().getFrequencia() / 1000000000);
+                        System.out.println(" Hz");
+                    }
+                    case 5 -> {
+                        System.out.println("Opção (Todos) escolhida! ");
+                        long discoTotal = looca.getGrupoDeDiscos().getVolumes().get(0).getTotal() / 1000000000;
+                        long discoDisponivel = looca.getGrupoDeDiscos().getVolumes().get(0).getDisponivel() / 1000000000;
+                        System.out.print("Porcentagem de uso do disco: ");
+                        System.out.print((discoTotal - discoDisponivel) * 100 / discoTotal);
+                        System.out.println(" %");
+                        long memoriaTotal = looca.getMemoria().getTotal() / 100000000;
+                        long memoriaEmUso = looca.getMemoria().getEmUso() / 100000000;
+                        System.out.print("Porcentagem de uso da memória: ");
+                        System.out.print(memoriaEmUso * 100 / memoriaTotal);
+                        System.out.println(" %");
+                        System.out.print("Bytes enviados: ");
+                        System.out.print(looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getBytesEnviados());
+                        System.out.println(" bytes");
+                        System.out.print("Bytes recebidos: ");
+                        System.out.print(looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getBytesRecebidos());
+                        System.out.println(" bytes");
+                        System.out.print("Frequência de CPU: ");
+                        System.out.print(looca.getProcessador().getFrequencia() / 1000000000);
+                        System.out.println(" Hz");
+                    }
+                    case 6 -> {
+                        System.out.println("Nome Processo; PID; % Uso CPU; % Uso RAM; MB utilizados; VRAM Utilizada");
+                        ProcessoGrupo prcs = looca.getGrupoDeProcessos();
+                        for (com.github.britooo.looca.api.group.processos.Processo processo : prcs.getProcessos()) {
+                            System.out.printf("%s; %d; %.1f; %.1f; %.1f\n",
+                                    processo.getNome(),
+                                    processo.getPid(),
+                                    processo.getUsoCpu(),
+                                    processo.getUsoMemoria(),
+                                    processo.getBytesUtilizados() / Math.pow(10, 6),
+                                    processo.getMemoriaVirtualUtilizada() / Math.pow(10, 6)
+                            );
+                        }
+                    }
+                    case 7 -> {
+                        System.out.println("Até mais!");
+                        Thread.currentThread().stop();
+                    }
+                    default -> {
+                        System.out.println("Escolha inválida!");
+                    }
                 }
 
                 try {
@@ -182,19 +158,19 @@ public class Terminal {
 
 
                 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-            } while (escolhadoDoMenu.getEscolhaDoMenu() != 7);
+            } while (escolhadoDoMenu != 7);
 
         } else {
             System.out.println("""
                     Usuário não encontrado!
                     
-                    Escolha uma opção: 
+                    Escolha uma opção:
                     1. Tentar novamente
                     2. Sair
                     """);
 
             System.out.print("Opção: ");
-            escolhadoDoMenu.setEscolhaDoMenu(leitor.nextInt() + 4);
+            escolhadoDoMenu = leitor.nextInt() + 4;
             leitor.nextLine();
         }
 
@@ -204,7 +180,7 @@ public class Terminal {
 
     public static Runnable capturaDeDados = () -> {
 
-        while (escolhadoDoMenu.getEscolhaDoMenu() != 7) {
+        while (escolhadoDoMenu != 7) {
             try {
                 Thread.sleep(8000);
             } catch (InterruptedException e) {
